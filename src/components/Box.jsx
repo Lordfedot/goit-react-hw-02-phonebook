bf0504e1dd7db9c932026675b02449c7aa798955
Lordfedot <orestmazgal@gmail.com>
2022-12-11 17:20:52 +0200

    done

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,8 +1,22 @@
-import styled from "styled-components";
-import { color,typography ,space,layout,flexbox,border,position } from "styled-system";
+import styled from 'styled-components';
+import {
+  color,
+  typography,
+  space,
+  layout,
+  flexbox,
+  border,
+  position,
+} from 'styled-system';
 
 const Box = styled('div')(
-    color,space,layout,flexbox,border,position,typography 
-)
+  color,
+  space,
+  layout,
+  flexbox,
+  border,
+  position,
+  typography
+);
 
-export default Box
+export default Box;
--- src/components/Phonebook/Filter.jsx
+++ src/components/Phonebook/Filter.jsx
@@ -1,13 +1,17 @@
-import {Input} from './Styled/Filter.styled'
+import { Input } from './Styled/Filter.styled';
 
-const Filter = ({filter,onChangeFilter}) => {
-    return (
-        <label>
-            Find contacts by name
-            <Input onChange={onChangeFilter} type="text" name="filter" value={filter} />
-        </label>
-    )
-}
+const Filter = ({ filter, onChangeFilter }) => {
+  return (
+    <label>
+      Find contacts by name
+      <Input
+        onChange={onChangeFilter}
+        type="text"
+        name="filter"
+        value={filter}
+      />
+    </label>
+  );
+};
 
-export default Filter
-
+export default Filter;
